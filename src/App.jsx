a2a49559aeb8a70ac633e90b56c9ea2351eee9c2
Your Name <you@example.com>
2025-09-11 10:30:59 -0400

    Lab 05 Draft 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,66 @@ import './App.css'
 import Header from './components/Header.jsx'
 import Introduction from './components/Introduction.jsx'
 import Card from './components/Card.jsx'
+import { useState } from 'react';
+
 
 
 
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filterRole, setFilterRole] = useState("");
+
+
   const cards = [
-    { title: "Project 1", description: "This is my first project." },
-    { title: "Project 2", description: "This is my second project." },
-    { title: "Project 3", description: "This is my third project." },
+    { title: "John Doe", description: "Web Developer" },
+    { title: "Joshua Tseitlin", description: "UX Designer" },
+    { title: "Ava Smith", description: "UI Designer" },
   ];
   
   return (
     <div className="app">
       <Header />
+
       <section id="home">
       <Introduction />
       </section>
+
       <section id="about">
         {/*about section*/}
       </section>
-      <section id="projects">
-        <div className="card-container">
-          {cards.map((card, index) => (
-            <Card key={index} title={card.title} description={card.description} />
-          ))}
-        </div>
+
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search profiles by name..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+
+        <select value={filterRole} onChange={(e) =>setFilterRole(e.target.value)}>
+          <option value={""}>All Roles</option>
+          <option value={"Web Developer"}>Web Developer</option>
+          <option value={"UX Designer"}>UX Designer</option>
+          <option value={"UI Designer"}>UI Designer</option>
+
+        </select>
+      </div>
+
+      <section id="profiles">
+      <div className="card-container">
+        {cards
+            .filter(card => 
+              card.title.toLowerCase().includes(searchTerm.toLowerCase()) && // filter by name
+              (filterRole === "" || card.description === filterRole)        // filter by position/description
+            )
+            .map((card, index) => (
+              <Card key={index} title={card.title} description={card.description} />
+            ))
+        }
+      </div>
       </section>
+
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
         <ul className="nav-links">
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
+          <li><a href="#profiles">Profiles</a></li>
         </ul>
       </nav>
 
