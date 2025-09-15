140ff9bc1a24d98b550d69372e7d3d41f8e6494e
Your Name <you@example.com>
2025-09-15 14:08:49 -0400

    Lab 06 Modes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,9 @@ function Header({ toggleMode, mode }) {
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#profiles">Profiles</a></li>
+          <button onClick={toggleMode} className="mode-toggle">{mode === "light" ? "Dark Mode" : "Light Mode"}</button>
         </ul>
-        <button onClick={toggleMode} className="mode-toggle">
-          {mode === "light" ? "Dark Mode" : "Light Mode"}
-        </button>
+
       </nav>
 
     </header>
